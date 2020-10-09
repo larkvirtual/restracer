ee36586a496202f8373793bcbbf75c9535cc52e6
Vladimir Gorelov <vg@iskod.ru>
2020-10-09 15:56:00 +0700

    Unmark version 0.A.04

--- libs/libtplreader/art_defs.h
+++ libs/libtplreader/art_defs.h
@@ -5,7 +5,7 @@
 #define ART_VENDOR_DATE "2002-2014"
 #define ART_VENDOR "Vladimir Gorelov"
 #define ART_VERSION_BRANCH "0.A"
-#define ART_VERSION_SUB ".04"
+#define ART_VERSION_SUB ""
 #define ART_VERSION_EXTRA ""
 #define ART_NUMBER_ARTUINT64TOSTR 1 /* :) */
 
