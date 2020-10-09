48ed68d7e185130f14412e0e0f061ce5c2a4a0cd
Vladimir Gorelov <vg@iskod.ru>
2020-10-09 15:42:46 +0700

    Dump branch version

--- libs/libtplreader/art_defs.h
+++ libs/libtplreader/art_defs.h
@@ -4,7 +4,7 @@
 #define ART_DEVELNAME "RESURRECTION"
 #define ART_VENDOR_DATE "2002-2014"
 #define ART_VENDOR "Vladimir Gorelov"
-#define ART_VERSION_BRANCH "CURRENT"
+#define ART_VERSION_BRANCH "0.A"
 #define ART_VERSION_SUB ""
 #define ART_VERSION_EXTRA ""
 #define ART_NUMBER_ARTUINT64TOSTR 1 /* :) */
