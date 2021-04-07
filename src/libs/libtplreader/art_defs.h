a0073c74191accb8b8a3cea798121cb8a0ee35f4
Vladimir Gorelov <vg@iskod.ru>
2021-04-07 13:30:13 +0700

    Create branch 0.B

--- src/libs/libtplreader/art_defs.h
+++ src/libs/libtplreader/art_defs.h
@@ -2,9 +2,9 @@
 #define ART_DEFS_H_
 
 #define ART_DEVELNAME "RESURRECTION"
-#define ART_VENDOR_DATE "2002-2014"
+#define ART_VENDOR_DATE "2002-2021"
 #define ART_VENDOR "Vladimir Gorelov"
-#define ART_VERSION_BRANCH "CURRENT"
+#define ART_VERSION_BRANCH "0.B"
 #define ART_VERSION_SUB ""
 #define ART_VERSION_EXTRA ""
 #define ART_NUMBER_ARTUINT64TOSTR 1 /* :) */
